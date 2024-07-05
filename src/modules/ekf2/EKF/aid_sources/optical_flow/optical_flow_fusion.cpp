e6a25690626c9f0181517138fdd4ab233010eafb
Daniel Agar <daniel@agar.ca>
2024-07-05 12:43:35 -0400

    ekf2: optical flow update last hor vel timestamp on success

--- src/modules/ekf2/EKF/aid_sources/optical_flow/optical_flow_fusion.cpp
+++ src/modules/ekf2/EKF/aid_sources/optical_flow/optical_flow_fusion.cpp
@@ -98,6 +98,8 @@ bool Ekf::fuseOptFlow(VectorState &H, const bool update_terrain)
 		_aid_src_optical_flow.time_last_fuse = _time_delayed_us;
 		_aid_src_optical_flow.fused = true;
 
+		_time_last_hor_vel_fuse = _time_delayed_us;
+
 		return true;
 	}
 
